09755f485dcd20519455c8c8cfc3ea11f8615273
rahmahi <rahamanmahbub17@yahoo.com>
2021-04-09 09:01:44 +0530

    cannonical commutation

---
+++ cannonical_commutation/AAA_Sympy_Quantum_Mechanics.py
@@ -0,0 +1,32 @@
+from __future__ import division
+import sys
+sys.path.insert(0, "~/.local/lib/python3.6/site-packages")
+
+import sympy
+from sympy import *
+from sympy.physics.quantum import *
+
+hbar = Symbol('hbar')
+
+x = Operator('x')
+p = Operator('p')
+ccr = Eq( Commutator(x, p), I*hbar )
+
+expr = x * (x**2 * p * x**2 + 1)
+expr.expand().apply_ccr(ccr)
+# => 3iħ x^4 + p x^5 + x
+
+
+H = Operator('H')
+a = Operator('a')
+ad = a.adjoint()
+n = Symbol('n')
+O = Symbol('Omega')
+
+up     = Eq( ad*Ket(n), sqrt(n+1)*Ket(n+1) )
+down   = Eq( a *Ket(n), sqrt(n)  *Ket(n-1) )
+energy = Eq( H *Ket(n), hbar*O*( n + 1/2 )*Ket(n) )
+
+expr = a * H * ad * Ket(n)
+expr.apply_operator(up, down, energy)
+# => Ωℏn^2 |n⟩ + 2.5Ωℏn |n⟩ + 1.5Ωℏ |n⟩
---
+++ cannonical_commutation/readme.txt
@@ -0,0 +1,5 @@
+here i have tried to intriduce the position and momentum operator and the commutation relations in quantum mechanics.
+
+ref:
+
+[1] https://gist.github.com/m93a/ff5abdd93a558f9e7c91c154179d06fc
